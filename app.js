d482fe3398d641708f3a37a1ea7727bad05383f5
Ben Halverson <benhalverson33@gmail.com>
2015-09-16 16:44:45 -0700

    refactor

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 'use strict';
 
 angular.module('githubUsers', []);
-angular.module('githubUsers')
-  .controller('mainCtrl', function($scope){
-  $scope.user = {
-    name: 'Ben',
-    email: 'benhalverson@me.com',
-    address:{street: '123 abc st',
-    city: 'Fremont',
-    state: 'CA'},
-    friends: ['a', 'b', 'c']
-  }
 
-});
 
 angular.module('githubUsers').directive('userInfoCard', function(){
   return {
     templateUrl: 'userinfocard.html',
-    restrict: 'E'
-  }
-})
+    restrict: 'E',
+    controller: function($scope) {
+      $scope.knightMe = function(user) {
+        user.rank = "Knighted";
+      };
+    }
+  };
+});
--- scripts/mainCtrl.js
+++ scripts/mainCtrl.js
@@ -1,3 +1,11 @@
-// angular.module('githubUsers').controller('mainCtrl', function($scope)){
-//
-// }
+angular.module('githubUsers').controller('mainCtrl', function($scope){
+  $scope.user = {
+    name: 'Ben',
+    email: 'benhalverson@me.com',
+    address:{street: '123 abc st',
+    city: 'Fremont',
+    state: 'CA'},
+    friends: ['a', 'b', 'c']
+  }
+
+});
