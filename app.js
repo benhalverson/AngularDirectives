9a25d8fb078caf80c4bc4ea6dd90f1e9a977154a
Ben Halverson <benhalverson33@gmail.com>
2015-09-16 17:31:58 -0700

    refactor

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-'use strict';
 
 angular.module('githubUsers', []);
 
 
-angular.module('githubUsers').directive('userInfoCard', function(){
+angular.module('githubUsers').directive('userInfoCard', function() {
   return {
-    templateUrl: 'userinfocard.html',
-    restrict: 'E',
+    templateUrl: "userInfoCard.html",
+    restrict: "E",
+    scope: {
+      user: '='
+    },
     controller: function($scope) {
+      $scope.collapsed = false;
       $scope.knightMe = function(user) {
-        user.rank = "Knighted";
-      };
+        user.rank = "knight";
+      }
+      $scope.collapse = function() {
+        $scope.collapsed = !$scope.collapsed;
+      }
+  console.log('Directive Userinfo Scope ', $scope.user);
     }
-  };
+
+  }
 });
+
+angular.module('githubUsers').directive('address', function() {
+  return {
+    restrict: 'E',
+    scope: true,
+    templateUrl: 'address.html',
+    controller: function($scope) {
+      $scope.collapsed = false;
+      $scope.collapseAddress = function() {
+        $scope.collapsed = true;
+      }
+      $scope.expandAddress = function() {
+        $scope.collapsed = false;
+      }
+console.log('Address Scope ', $scope.user.address);
+    }
+
+  }
+})
--- scripts/mainCtrl.js
+++ scripts/mainCtrl.js
@@ -1,11 +1,29 @@
 angular.module('githubUsers').controller('mainCtrl', function($scope){
-  $scope.user = {
-    name: 'Ben',
-    email: 'benhalverson@me.com',
-    address:{street: '123 abc st',
-    city: 'Fremont',
-    state: 'CA'},
-    friends: ['a', 'b', 'c']
+  $scope.user1 = {
+    name: 'Luke Skywalker',
+    address: {
+      street: 'PO Box 123',
+      city: 'Secret Rebel Base',
+      planet: 'Yavin 4'
+    },
+    friends: [
+      'Han',
+      'Leia',
+      'Chewbacca'
+    ]
   }
-
+  $scope.user2 = {
+    name: 'Han Solo',
+    address: {
+      street: 'PO Box 123',
+      city: 'Mos Eisley',
+      planet: 'Tattoine'
+    },
+    friends: [
+      'Han',
+      'Leia',
+      'Chewbacca'
+    ]
+  }
+console.log('Main Controller Scope ', $scope);
 });
